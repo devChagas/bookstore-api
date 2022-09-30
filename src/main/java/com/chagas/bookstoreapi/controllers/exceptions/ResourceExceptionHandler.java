7c6884dd4fec92e3cdbbcb2533ccb9a8afbd03f1
devChagas <gabriel.chagas2044@gmail.com>
2022-09-30 11:55:53 -0300

    Excecao personalizada para objeto nao encontrado

---
+++ src/main/java/com/chagas/bookstoreapi/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.chagas.bookstoreapi.controllers.exceptions;
+
+import com.chagas.bookstoreapi.services.exceptions.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.ServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandartError> objectNotFoundException(ObjectNotFoundException e, ServletRequest request) {
+        StandartError error = new StandartError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
