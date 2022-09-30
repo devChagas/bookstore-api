2c96e56c4d0afc9a69e21f6438eda0d53d22b689
devChagas <gabriel.chagas2044@gmail.com>
2022-09-30 14:22:21 -0300

    POST para Categoria

--- src/main/java/com/chagas/bookstoreapi/controllers/CategoriaController.java
+++ src/main/java/com/chagas/bookstoreapi/controllers/CategoriaController.java
@@ -6,7 +6,10 @@ import com.chagas.bookstoreapi.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.Servlet;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,6 +30,12 @@ public class CategoriaController {
         List<Categoria> list = categoriaService.findAll();
         List<CategoriaDTO> listDTO = list.stream().map(CategoriaDTO::new).toList();
         return ResponseEntity.ok().body(listDTO);
+    }
 
+    @PostMapping
+    public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
+        obj = categoriaService.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
