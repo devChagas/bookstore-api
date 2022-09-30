3c1baffb36cc351d41d1e7adc35434f2440fe2f3
devChagas <gabriel.chagas2044@gmail.com>
2022-09-30 15:00:59 -0300

    Listando todos os Livros por Categoria

--- src/main/java/com/chagas/bookstoreapi/controllers/LivroController.java
+++ src/main/java/com/chagas/bookstoreapi/controllers/LivroController.java
@@ -2,13 +2,14 @@ package com.chagas.bookstoreapi.controllers;
 
 
 import com.chagas.bookstoreapi.domains.Livro;
+import com.chagas.bookstoreapi.dtos.LivroDTO;
 import com.chagas.bookstoreapi.services.LivroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/livros")
@@ -20,6 +21,13 @@ public class LivroController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Livro> findById(@PathVariable Integer id) {
         Livro obj = livroService.findById(id);
-        return ResponseEntity.ok().body(obj); 
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat) {
+        List<Livro> list = livroService.findAll(id_cat);
+        List<LivroDTO> listDTO = list.stream().map(LivroDTO::new).toList();
+        return ResponseEntity.ok().body(listDTO);
     }
 }
--- src/main/java/com/chagas/bookstoreapi/services/LivroService.java
+++ src/main/java/com/chagas/bookstoreapi/services/LivroService.java
@@ -7,6 +7,7 @@ import org.aspectj.apache.bcel.classfile.Module;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,9 +16,17 @@ public class LivroService {
     @Autowired
     private LivroRepository livroRepository;
 
+    @Autowired
+    private CategoriaService categoriaService;
+
     public Livro findById(Integer id){
         Optional<Livro> obj = livroRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id +
                 ", Tipo: " + Livro.class.getName()));
+    }
+
+    public List<Livro> findAll(Integer id_cat) {
+        categoriaService.findById(id_cat);
+        return livroRepository.findAllByCategoria(id_cat);
     }
 }
