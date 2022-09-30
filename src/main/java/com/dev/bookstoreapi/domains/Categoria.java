6b123b30a10e51a8d1069f11cb0d6c6df83bfe75
devChagas <gabriel.chagas2044@gmail.com>
2022-09-30 10:22:54 -0300

    finalizando as classes de domínio

--- src/main/java/com/dev/bookstoreapi/domains/Categoria.java
+++ src/main/java/com/dev/bookstoreapi/domains/Categoria.java
@@ -1,18 +1,25 @@
 package com.dev.bookstoreapi.domains;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 @Entity
-public class Categoria {
+public class Categoria implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String descricao;
 
+    @OneToMany(mappedBy = "categoria")
     private List<Livro> livros = new ArrayList<>();
+
 
     public Categoria() {
     }
